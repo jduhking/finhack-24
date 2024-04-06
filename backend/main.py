88f85ccc51c90d32b8fe85778032fdc8345f72f8
jduhking <jamesbodebiyi@gmail.com>
2024-04-06 02:36:51 -0500

    set up the reading of xml files

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import requests
-
+import pandas as pd
 
 
 app = FastAPI()
@@ -15,27 +15,24 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
+market_to_url = {
+    "tech": "https://news.google.com/rss/search?q=when:24h+allinurl:bloomberg.com&hl=en-US&gl=US&ceid=US:en",
+}
 
-def fetch_xml_data(market: str) -> str:
-    # this function will fetch the xml data depending on the market
-    market_to_url = {
-        "tech": "https://news.google.com/rss/search?q=when:24h+allinurl:bloomberg.com&hl=en-US&gl=US&ceid=US:en",
-    }
-    # get the xml feed url corresponding to the specified market
-
-    xml_url = market_to_url[market]
+def fetch_xml_data(market: str) -> bytes:
+    # Get the XML feed URL corresponding to the specified market
+    xml_url = market_to_url.get(market)
 
     if not xml_url:
         raise HTTPException(status_code=404, detail="XML feed URL not found for market")
-    
-    # fetch the XML data from the URL
 
-    response = requests.get(xml_url)
-
-    if response.status_code != 200:
-        raise HTTPException(status_code=500, detail=f"Failed to fetch XML data: { response.text }")
-    
-    return response.content
+    # Fetch the XML data from the URL
+    try:
+        response = requests.get(xml_url)
+        response.raise_for_status()  # Raise an error for non-200 status codes
+        return response.content  # Return the raw bytes content of the response
+    except requests.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Failed to fetch XML data: {str(e)}")
 
 @app.get("/")
 async def root():
@@ -49,7 +46,13 @@ async def invest(request: Request):
     market = payload["market"]
 
     xml = fetch_xml_data(market)
+    
+    df = pd.read_xml(xml, xpath=".//item")
 
-    print(xml)
+    df['title']
+    print(df['title'])
+    return ""
 
-
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
