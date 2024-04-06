11a4038e9f1b10fea1a75591185b3fd070f4f260
jduhking <jamesbodebiyi@gmail.com>
2024-04-06 04:34:25 -0500

    parse through bloomberg and get headlines

--- backend/main.py
+++ backend/main.py
@@ -28,8 +28,12 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
-market_to_url = {
-    "tech": "https://news.google.com/rss/search?q=when:24h+allinurl:bloomberg.com&hl=en-US&gl=US&ceid=US:en",
+market_to_urls = {
+    "tech": ["https://news.google.com/rss/search?q=when:336h+allinurl:bloomberg.com&hl=en-US&gl=US&ceid=US:en",],
+}
+
+market_to_companies = {
+    "tech" : ["microsoft", "apple", "nvidia", "google", "amazon" ]
 }
 def preprocess_data(title):
     # Tokenize the title
@@ -47,17 +51,29 @@ def preprocess_data(title):
  
     return preprocessed_title
 
+def preprocess_tokens(title):
+        # Tokenize the title
+    words = word_tokenize(title)
 
-def fetch_xml_data(market: str) -> bytes:
+    # Remove stopwords and punctuation
+    words = [word for word in words if word not in stop_words and word not in string.punctuation]
+
+    # Convert the words to lowercase
+    words = [word.lower() for word in words]
+    return words
+
+
+
+
+def fetch_xml_data(url) -> bytes:
     # Get the XML feed URL corresponding to the specified market
-    xml_url = market_to_url.get(market)
 
-    if not xml_url:
+    if not url:
         raise HTTPException(status_code=404, detail="XML feed URL not found for market")
 
     # Fetch the XML data from the URL
     try:
-        response = requests.get(xml_url)
+        response = requests.get(url)
         response.raise_for_status()  # Raise an error for non-200 status codes
         return response.content  # Return the raw bytes content of the response
     except requests.RequestException as e:
@@ -74,23 +90,34 @@ async def invest(request: Request):
     strategy = payload["strategy"]
     market = payload["market"]
 
-    xml = fetch_xml_data(market)
-    
-    df = pd.read_xml(xml, xpath=".//item")
+    xml_urls = market_to_urls.get(market)
+    companies = market_to_companies.get(market)
 
-    titles = df['title'].values
-    scores_dict = {}
-    for title in titles:
-        # tokenize each title
-        pre = preprocess_data(title)
-        # get the score
-        scores = sia.polarity_scores(pre)
-        scores_dict[pre] = scores
-    print(scores_dict)
+    for url in xml_urls:
+        xml = fetch_xml_data(url)
+        df = pd.read_xml(xml, xpath=".//item")
+        titles = df['title'].values
+        # for each headline, map it to a company
+        headlines_to_company = {}
+        for company in companies:
+            headlines_to_company[company] = []
+        scores_dict = {}
+        for title in titles:
+            # tokenize each title
+            tokens = preprocess_tokens(title)
+            headline = preprocess_data(title)
+            # check each of the words and see if they contain any of our companies, if not discard
+            for token in tokens:
+                for company in companies:
+                    if token == company:
+                        headlines_to_company[company].append(headline)
+            
+        print(headlines_to_company)
 
-    return scores_dict
+    return ""
 
- 
+            #  scores = sia.polarity_scores(pre)
+            # scores_dict[pre] = scores
 
 if __name__ == "__main__":
     import uvicorn
